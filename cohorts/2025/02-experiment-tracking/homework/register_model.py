cae7fe9cbeee978564076b17d09e8490baf4cb94
Kristina <kristina.a.albrecht@gmail.com>
2025-05-27 10:59:40 +0000

    Homework 2: register model

--- cohorts/2025/02-experiment-tracking/homework/register_model.py
+++ cohorts/2025/02-experiment-tracking/homework/register_model.py
@@ -6,13 +6,15 @@ import mlflow
 from mlflow.entities import ViewType
 from mlflow.tracking import MlflowClient
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import root_mean_squared_error
 
 HPO_EXPERIMENT_NAME = "random-forest-hyperopt"
 EXPERIMENT_NAME = "random-forest-best-models"
 RF_PARAMS = ['max_depth', 'n_estimators', 'min_samples_split', 'min_samples_leaf', 'random_state']
+TRACKING_URI = "http://127.0.0.1:5000"
+MODEL_NAME = "nyc-taxi-trip-duration-rf"
 
-mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_tracking_uri(TRACKING_URI)
 mlflow.set_experiment(EXPERIMENT_NAME)
 mlflow.sklearn.autolog()
 
@@ -36,9 +38,9 @@ def train_and_log_model(data_path, params):
         rf.fit(X_train, y_train)
 
         # Evaluate model on the validation and test sets
-        val_rmse = mean_squared_error(y_val, rf.predict(X_val), squared=False)
+        val_rmse = root_mean_squared_error(y_val, rf.predict(X_val))
         mlflow.log_metric("val_rmse", val_rmse)
-        test_rmse = mean_squared_error(y_test, rf.predict(X_test), squared=False)
+        test_rmse = root_mean_squared_error(y_test, rf.predict(X_test))
         mlflow.log_metric("test_rmse", test_rmse)
 
 
@@ -71,10 +73,24 @@ def run_register_model(data_path: str, top_n: int):
 
     # Select the model with the lowest test RMSE
     experiment = client.get_experiment_by_name(EXPERIMENT_NAME)
-    # best_run = client.search_runs( ...  )[0]
+    runs = client.search_runs(
+        experiment_ids=experiment.experiment_id,
+        run_view_type=ViewType.ACTIVE_ONLY,
+        max_results=1,
+        order_by=["metrics.test_rmse ASC"]
+    )
+    if not runs:
+        print("No runs found in the experiment.")
+        return
+    best_run = runs[0]
+    print(f"Best run ID: {best_run.info.run_id}")
+    print(f"Best run test RMSE: {best_run.data.metrics['test_rmse']}")
+    print(f"Best run parameters: {best_run.data.params}")
 
     # Register the best model
-    # mlflow.register_model( ... )
+    model_uri = f"runs:/{best_run.info.run_id}/model"
+    mlflow.register_model(model_uri=model_uri, name=MODEL_NAME)
+
 
 
 if __name__ == '__main__':
